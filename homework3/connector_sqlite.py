09615de4d481ab55b61a7101ea5f92aa2cb3dae8
Andrii <andriy201717@gmail.com>
2025-03-23 14:03:34 +0100

    add new strings

--- homework3/connector_sqlite.py
+++ homework3/connector_sqlite.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Integer, String, ForeignKey, Numeric, Boolean
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from decimal import Decimal
 
 URL = "sqlite:///:memory:"
 
@@ -7,23 +8,29 @@ engine = create_engine(URL, echo=True, echo_pool=True)
 
 Base = declarative_base()
 
+
+class Category(Base):
+    __tablename__ = "category"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(String(255))
+
+    products: Mapped[list["Product"]] = relationship("Product", back_populates="category")
+
+
 class Product(Base):
     __tablename__ = "product"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     category_id: Mapped[int] = mapped_column(Integer, ForeignKey("category.id"))
     name: Mapped[str] = mapped_column(String(100))
-    price: Mapped[float] = mapped_column(Numeric(5,2))
-    in_stock: Mapped[bool] = mapped_column(Boolean, default=True, server_default="in stock")
-    category: Mapped["Category"] = relationship("Category", "id")
+    price: Mapped[Decimal] = mapped_column(Numeric(5, 2))
+    in_stock: Mapped[bool] = mapped_column(Boolean, default=True, server_default="1")
 
-class Category(Base):
-    __tablename__ = "category"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, )
-    name: Mapped[str] = mapped_column(String(100))
-    description: Mapped[str] = mapped_column(String(255))
-    product: Mapped["Product"] = relationship("Product", "category_id")
+    category: Mapped["Category"] = relationship("Category", back_populates="products")
+
 
 Base.metadata.create_all(engine)
+
 
 
 
